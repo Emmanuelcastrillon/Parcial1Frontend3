8eb965bbf1b08b9e88c9439890e5298db6288ace
unknown <castrillon.rivas@gmail.com>
2023-11-02 18:43:15 -0500

    primer parcial en vivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
+import { useState } from 'react'
 import Card from './Components/Card'
+import styleApp from "./Components/card.module.css"
 
 function App() {
-  
+
+  const [nombre,setNombre]=useState('')
+  const [cancion,setCancion]=useState('')
+  const [genero,setGenero]=useState('')
+  const [validacion,setValidacion]=useState(false)
+  const [error,setError]=useState(false)
+
+  const handleNombre=(event)=>setNombre(event.target.value.trimStart())
+  const handleCancion=(event)=>setCancion(event.target.value.trimStart())
+  const handleGenero=(event)=>setGenero(event.target.value.trimStart())
+
+  const handleSubmit=(event)=>{
+    event.preventDefault()
+    if(nombre.length>=3 && cancion.length>=3  && genero.length>=6){
+      setValidacion(true)
+      setError(false)
+     
+    }
+    else{
+      setValidacion(false)
+      setError(true)
+    }
+  }
 
   return (
-    <>
-    <h1>Carga de estudiantes</h1>
-     <form ></form>
-     <Card/>
-    </>
+    <div>
+      { !validacion&&
+          <form className={styleApp.formulario} onSubmit={handleSubmit} >
+          <h1 className={styleApp.titulo}>Escribe tu cancion favorita</h1>
+             <input className={styleApp.inputs} type="text" placeholder='Escribe tu nombre' onChange={handleNombre}/>
+             <input className={styleApp.inputs} type="text" placeholder='Escribe tu cancion favorita' onChange={handleCancion} />
+             <input className={styleApp.inputs}type="text" placeholder='Escribe el genero de tu cancion' onChange={handleGenero} />
+             <button className={styleApp.boton}>Enviar</button>
+          </form>
+      }
+   
+
+     {validacion?   <Card nombre={nombre} cancion={cancion} genero={genero}/>:
+      null
+     }
+  
+   
+     {error&& <h3 className={styleApp.mensajeError}>Por favor chequea que la información sea correcta</h3>}
+    </div>
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styleCard from "../Components/card.module.css"
 
-const Card = () => {
+const Card = ({nombre,cancion,genero}) => {
   return (
-    <div>Hola, soy una tarjeta</div>
+    <div className={styleCard.contenedor}>
+      <h1 className={styleCard.tituloCard}>Hola {nombre}</h1>
+      <h2 className={styleCard.cancion}>Tu cancion favorita es "{cancion}"</h2>
+      <h2 className={styleCard.genero}>El genero de la cancion ({cancion}) es "{genero}"</h2>
+    </div>
   )
 }
 
